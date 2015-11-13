d2c2c85b9a5c0521067fc79bb9787564bc8de3e2
Andrea Balducci <mtb.snowboard@gmail.com>
2015-11-13 09:33:53 +0100

    enabled command line options

--- src/Jarvis.TypeScriptGenerator/Program.cs
+++ src/Jarvis.TypeScriptGenerator/Program.cs
@@ -1,15 +1,197 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using Jarvis.TypeScriptGenerator.Builders;
+using Newtonsoft.Json;
 
 namespace Jarvis.TypeScriptGenerator
 {
     class Program
     {
-        static void Main(string[] args)
+        private const string TsgeneratorJson = "tsgenerator.json";
+        private static string _workingFolder;
+        private static string _tsGeneratorJson;
+        private static bool _optionInit;
+        private static bool _optionRun;
+
+        static int Main(string[] args)
         {
+            Setup();
+
+            if (ParseArgs(args) == 0)
+            {
+                Banner();
+                return 0;
+            }
+
+            if (_optionInit)
+            {
+                return Init();
+            }
+
+            if (_optionRun)
+            {
+                return Run();
+            }
+
+            return 0;
+        }
+
+        private static int Run()
+        {
+            if (!ExistsTsGeneratorJson())
+            {
+                Console.WriteLine("File {0} is missing", TsgeneratorJson);
+                return 0;
+            }
+
+            try
+            {
+                var text = File.ReadAllText(_tsGeneratorJson);
+                var options = JsonConvert.DeserializeObject<TypeScriptGeneratorOptions>(text);
+
+                foreach (var pathToAssembly in options.Assemblies)
+                {
+                    ProcessAssembly(pathToAssembly, options);
+                }
+            }
+            catch (Exception ex)
+            {
+                DumpError(ex);
+                return -2;
+            }
+
+            return 1;
+        }
+
+        private static bool ProcessAssembly(string pathToAssembly, TypeScriptGeneratorOptions options)
+        {
+            if (!File.Exists(pathToAssembly))
+            {
+                Console.WriteLine("File {0} not found", pathToAssembly);
+                return false;
+            }
+
+            try
+            {
+                var a = Assembly.LoadFile(pathToAssembly);
+                var controllers = a.GetTypes().Where(x =>
+                    !x.IsAbstract &&
+                    x.IsClass &&
+                    typeof(IHttpController).IsAssignableFrom(x)
+
+                ).ToArray();
+
+                if (!controllers.Any())
+                {
+                    Console.WriteLine("No controllers found");
+                    return false;
+                }
+
+                foreach (var controller in controllers)
+                {
+                    var builder = new TypeScriptBuilder(options.DestFolder, options.NgModule);
+                    foreach (var reference in options.References)
+                    {
+                        builder.AddReference(reference);
+                    }
+
+                    var pathToTs = builder.GenerateClientApi(controller, options.Namespace, options.ApiRoot);
+                    Console.WriteLine("Written {0}", pathToTs);
+                }
+            }
+            catch (Exception ex)
+            {
+                DumpError(ex);
+                return false;
+            }
+
+            return true;
+        }
+
+        private static int ParseArgs(string[] args)
+        {
+            int parsed = 0;
+            foreach (var arg in args)
+            {
+                switch (Normalize(arg))
+                {
+                    case "init":
+                        _optionInit = true;
+                        parsed++;
+                        break;
+
+                    case "run":
+                        _optionRun = true;
+                        parsed++;
+                        break;
+                }
+            }
+
+            return parsed;
+        }
+
+        private static void Setup()
+        {
+            _workingFolder = AppDomain.CurrentDomain.BaseDirectory;
+            _tsGeneratorJson = Path.Combine(_workingFolder, TsgeneratorJson);
+            _optionInit = false;
+        }
+
+        private static int Init()
+        {
+            if (ExistsTsGeneratorJson())
+            {
+                Console.WriteLine("File {0} already exists", _tsGeneratorJson);
+                return 0;
+            }
+
+            Console.WriteLine("Writing {0}", _tsGeneratorJson);
+
+            var options = new TypeScriptGeneratorOptions();
+            try
+            {
+                options.Init(_workingFolder);
+                options.Save(_tsGeneratorJson);
+            }
+            catch (Exception ex)
+            {
+                DumpError(ex);
+                return -1;
+            }
+            return 1;
+        }
+
+        private static void DumpError(Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+
+        private static bool ExistsTsGeneratorJson()
+        {
+            return File.Exists(_tsGeneratorJson);
+        }
+
+        private static string Normalize(string s)
+        {
+            return s.ToLowerInvariant();
+        }
+
+        private static void Banner()
+        {
+            if (!Environment.UserInteractive)
+                return;
+
+            Console.WriteLine("================================================");
+            Console.WriteLine(" TypeScriptGenerator " + Assembly.GetExecutingAssembly().GetName().Version);
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine("  init         create a tsgenerator.json if not exists");
+            Console.WriteLine();
         }
     }
 }
---
+++ src/Jarvis.TypeScriptGenerator/TypeScriptGeneratorOptions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Jarvis.TypeScriptGenerator
+{
+    public class TypeScriptGeneratorOptions
+    {
+        public string DestFolder { get; set; }
+        public string[] Assemblies { get; set; }
+        public string NgModule { get; set; }
+        public string[] References { get; set; }
+        public string Namespace { get; set; }
+        public string ApiRoot { get; set; }
+
+        public TypeScriptGeneratorOptions()
+        {
+        }
+
+        public void Init(string workingFolder)
+        {
+            this.DestFolder = workingFolder;
+            this.Assemblies = new[] { "path/to/assembly.dll" };
+            this.References = new[] { "typings/tsd.d.ts" };
+            this.NgModule = "api";
+            this.ApiRoot = "http://localhost/api/v1/";
+            this.Namespace = "your.api";
+        }
+
+        public void Save(string pathToFile)
+        {
+            File.WriteAllText(pathToFile, JsonConvert.SerializeObject(this, Formatting.Indented));
+        }
+    }
+}
